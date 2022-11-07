a9fbbed60afad3431a4c4b1794b184bbf24c8f8f
Lojitao <Lojitao@outlook.com>
2022-11-07 18:41:02 +0800

    [update]QRcode功能完成

--- src/views/partnerstores/directAppStore.vue
+++ src/views/partnerstores/directAppStore.vue
@@ -16,48 +16,24 @@
       var android = ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1; // 判斷android裝置
       var iOS = !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // 判斷ios裝置
       
-      //let getUrlString = new URL('https://github.com/search?storeID=347942604415045');//測試用
-      // let getUrlString = new URL(location.href);//正式站
-      // window.alert(getUrlString)
-      // let urlQuery = getUrlString.searchParams.toString()
-      // window.alert(urlQuery)
-
-     
-      
-      // let storeID = "" ;
-      // let storePwd = "" ;
-      
-      
-      
       // if(android || iOS){
-        // let getUrlString = location.href;
-        //let queryArray = []
         //拆出query參數
         let getUrlString = location.href;
         let Ary1 = getUrlString.split('?');//把問號(?)後的參數拆出來
         let Ary2 = Ary1[1].split('=')//把 key value拆出來
         let storeID = "" ;
         let storePwd = "" ;
-        window.alert(getUrlString)
-        window.alert(Ary1)
-        window.alert(Ary2)
+        // window.alert(getUrlString)
+        // window.alert(Ary1)
+        // window.alert(Ary2)
         if(Ary2[0]==='storeID'){
           storeID = Ary2[1]
         }
         if(Ary2[0]==='storePwd'){
           storePwd = Ary2[1]
         }
-        window.alert(storeID)
-        window.alert(storePwd)
-        // if(urlQuery){
-        //   queryArray = urlQuery.split('=')
-        // }
-        // if(queryArray[0]==='storeID'){
-        //   storeID = queryArray[1]
-        // }
-        // if(queryArray[0]==='storePwd'){
-        //   storePwd = queryArray[1]
-        // }
+        // window.alert(storeID)
+        // window.alert(storePwd)
        
         if(storeID && android){
           window.location.replace(`${'https://play.google.com/store/apps/details?id=com.isushop.isu'}${'&storeID='}${storeID}`);
